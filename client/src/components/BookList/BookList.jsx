589526bc573bfe94c146a1368d34c7fcb1867b1b
john12321 <johnomeara72@gmail.com>
2019-02-21 01:10:40 +0000

    Added BookDetails component and graphql getBookQuery - next job: style everything!

--- client/src/components/BookList/BookList.jsx
+++ client/src/components/BookList/BookList.jsx
@@ -3,13 +3,23 @@ import "./BookList.css";
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../../queries/queries";
 
+import BookDetails from "../BookDetails/BookDetails";
+
 class BookList extends Component {
+  state = {
+    bookId: ""
+  };
+
   displayBooks = () => {
     const data = this.props.data;
     if (!data.loading) {
       return data.books.map(book => {
         return (
-          <li id="book-li" key={book.id}>
+          <li
+            id="book-li"
+            key={book.id}
+            onClick={() => this.handleClick(book.id)}
+          >
             {book.name}
           </li>
         );
@@ -17,11 +27,16 @@ class BookList extends Component {
     } else return <li>Loading books...</li>;
   };
 
+  handleClick = bookId => {
+    this.setState({ bookId: bookId });
+  };
+
   render() {
     return (
       <div>
         <h2>Book list</h2>
         <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails bookId={this.state.bookId} />
       </div>
     );
   }
